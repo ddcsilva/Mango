a4f1ee5150c38a8eb2b2eed89b02ad67dea8a395
Danilo Silva <danilo.silva@msn.com>
2025-03-19 23:24:38 -0300

    Implementa migrações e dados iniciais para cupons
    Modifica o construtor da classe `AppDbContext` para aceitar opções de contexto e adiciona dados iniciais para a tabela `Coupons`. Atualiza o `AppDbContextModelSnapshot.cs` com os dados da entidade `Coupon`. Adiciona a função `ApplyMigrations` no `Program.cs` para aplicar migrações pendentes ao iniciar a aplicação. Cria a migração `20250320022134_SeedCouponTable` para inserir dados na tabela `Coupons` e gera o arquivo de designer correspondente.

--- Mango.Services.CouponAPI/Data/AppDbContext.cs
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -3,10 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.CouponAPI.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-    }
     public DbSet<Coupon> Coupons { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Coupon>().HasData(
+            new Coupon { CouponId = 1, CouponCode = "10OFF", DiscountAmount = 10, MinAmount = 50 },
+            new Coupon { CouponId = 2, CouponCode = "20OFF", DiscountAmount = 20, MinAmount = 100 }
+        );
+    }
 }
--- Mango.Services.CouponAPI/Program.cs
+++ Mango.Services.CouponAPI/Program.cs
@@ -25,4 +25,16 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+ApplyMigrations(app);
+
 app.Run();
+
+static void ApplyMigrations(WebApplication app)
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    if (db.Database.GetPendingMigrations().Any())
+    {
+        db.Database.Migrate();
+    }
+}
