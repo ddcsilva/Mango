59f5600469b34e7c0ae10bcff8ef6eec30be155f
Danilo Silva <danilo.silva@msn.com>
2025-03-20 17:33:29 -0300

    Adiciona suporte a requisições HTTP com Newtonsoft.Json
    Inclui a referência ao pacote `Newtonsoft.Json` na versão `13.0.3` no projeto.
    Implementa a classe `BaseService` no arquivo `BaseService.cs`, que utiliza `IHttpClientFactory` para enviar requisições HTTP assíncronas.
    Adiciona tratamento para diferentes tipos de requisições (POST, PUT, DELETE, GET) e gerencia as respostas da API, retornando um objeto `ResponseDTO` com o status da operação.

---
+++ Mango.Web/Service/BaseService.cs
@@ -0,0 +1,74 @@
+﻿using Mango.Web.Dtos;
+using Mango.Web.DTOs;
+using Mango.Web.Enums;
+using Mango.Web.Interfaces;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+
+namespace Mango.Web.Service;
+
+public class BaseService : IBaseService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public BaseService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<ResponseDTO?> SendAsync(RequestDTO requestDTO)
+    {
+        try
+        {
+            var client = _httpClientFactory.CreateClient("MangoAPI");
+            var message = new HttpRequestMessage();
+
+            message.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            message.RequestUri = new Uri(requestDTO.Url);
+
+            if (requestDTO.Data != null)
+            {
+                message.Content = new StringContent(JsonConvert.SerializeObject(requestDTO.Data), Encoding.UTF8, "application/json");
+            }
+
+            switch (requestDTO.ApiType)
+            {
+                case ApiType.POST:
+                    message.Method = HttpMethod.Post;
+                    break;
+                case ApiType.PUT:
+                    message.Method = HttpMethod.Put;
+                    break;
+                case ApiType.DELETE:
+                    message.Method = HttpMethod.Delete;
+                    break;
+                default:
+                    message.Method = HttpMethod.Get;
+                    break;
+            }
+
+            var apiResponse = await client.SendAsync(message);
+
+            switch (apiResponse.StatusCode)
+            {
+                case HttpStatusCode.NotFound:
+                    return new ResponseDTO { IsSuccess = false, Message = "Not Found" };
+                case HttpStatusCode.Forbidden:
+                    return new ResponseDTO { IsSuccess = false, Message = "Access Denied" };
+                case HttpStatusCode.Unauthorized:
+                    return new ResponseDTO { IsSuccess = false, Message = "Unauthorized" };
+                case HttpStatusCode.InternalServerError:
+                    return new ResponseDTO { IsSuccess = false, Message = "Internal Server Error" };
+                default:
+                    var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                    var responseDto = JsonConvert.DeserializeObject<ResponseDTO>(apiContent);
+                    return responseDto;
+            }
+        }
+        catch (Exception ex)
+        {
+            return new ResponseDTO { IsSuccess = false, Message = ex.Message };
+        }
+    }
+}
