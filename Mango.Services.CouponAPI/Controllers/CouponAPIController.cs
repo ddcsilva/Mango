34ca3688d4096a02e81ac531d0a27103562cc085
Danilo Silva <danilo.silva@msn.com>
2025-03-19 23:34:01 -0300

    Remove previsão do tempo e adiciona gerenciamento de cupons
    As classes `WeatherForecastController` e `WeatherForecast` foram removidas, eliminando a funcionalidade de previsão do tempo.
    
    Foi adicionado o `CouponAPIController`, que gerencia cupons com métodos para obter todos os cupons e buscar um cupom específico pelo ID, utilizando o `AppDbContext` para interagir com a base de dados e tratamento de exceções para erros.

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,47 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Controllers;
+
+[Route("api/coupon")]
+[ApiController]
+public class CouponAPIController(AppDbContext context) : ControllerBase
+{
+    private readonly AppDbContext _context = context;
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Coupon>>> GetAllCoupons()
+    {
+        try
+        {
+            var coupons = await _context.Coupons.ToListAsync();
+            return Ok(coupons);
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, "Erro interno no servidor.");
+        }
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Coupon>> GetCouponById(int id)
+    {
+        try
+        {
+            var coupon = await _context.Coupons.FirstOrDefaultAsync(c => c.CouponId == id);
+
+            if (coupon == null)
+            {
+                return NotFound($"Cupom com ID {id} não encontrado.");
+            }
+
+            return Ok(coupon);
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, "Erro interno no servidor.");
+        }
+    }
+}
