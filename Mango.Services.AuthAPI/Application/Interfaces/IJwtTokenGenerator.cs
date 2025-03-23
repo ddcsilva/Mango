e5dc85584d2c9a450714843f1e1804bb265b1858
Danilo Silva <danilo.silva@msn.com>
2025-03-23 11:38:06 -0300

    Adiciona geração de tokens JWT com interface
    Implementa a interface `IJwtTokenGenerator` e a classe `JwtTokenGenerator` para gerar tokens JWT. A classe utiliza a biblioteca `Microsoft.IdentityModel.Tokens` e permite a personalização das opções de configuração através de injeção de dependência.

---
+++ Mango.Services.AuthAPI/Application/Interfaces/IJwtTokenGenerator.cs
@@ -0,0 +1,8 @@
+﻿using Mango.Services.AuthAPI.Domain.Models;
+
+namespace Mango.Services.AuthAPI.Application.Interfaces;
+
+public interface IJwtTokenGenerator
+{
+    string GenerateToken(ApplicationUser user);
+}
