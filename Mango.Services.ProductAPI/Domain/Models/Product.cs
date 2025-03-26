89df2df9f8ebc713a78b7638cb69961ea4a2875e
Danilo Silva <danilo.silva@msn.com>
2025-03-25 22:23:38 -0300

    Adiciona estrutura de pastas e classe Product
    Modifica o arquivo `Mango.Services.ProductAPI.csproj` para incluir pastas `Application`, `Controllers`, `Extensions` e `Infrastructure`. Mantém a referência ao pacote `Swashbuckle.AspNetCore`.
    
    Cria o arquivo `Product.cs` com a definição da classe `Product`, incluindo propriedades como `ProductId`, `Name`, `Price`, `Description`, `CategoryName` e `ImageUrl`, além de anotações de validação de dados.

---
+++ Mango.Services.ProductAPI/Domain/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.ProductAPI.Domain.Models;
+
+public class Product
+{
+    [Key]
+    public int ProductId { get; set; }
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; } = string.Empty;
+    public double Price { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string CategoryName { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+}
+
